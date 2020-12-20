ebddba11c7e0bed94895206375c15b7627afde3e
hp <hp@192.168.0.8>
2020-12-20 13:54:04 +0530

    Simplearn Project

---
+++ Pavan-Lokemee/src/com/model/FileInterface.java
@@ -0,0 +1,9 @@
+package com.model;
+
+public interface FileInterface {
+	public abstract void showAllFiles();
+	public abstract void addFile();
+	public abstract void deleteFile();
+	public abstract void searchFile();
+
+}
